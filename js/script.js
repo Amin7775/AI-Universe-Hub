ec2aa2ff7ff222edc571f5bd5c46b7c3b6d3c7de
Amin7775 <amin.dpi24@gmail.com>
2023-09-01 20:50:42 +0600

    added cards

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const loadData = async () => {
+    // Fetch data from the API
+    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+
+    const data = await res.json();
+
+    const details = data.data.tools;
+  
+    console.log(typeof details);
+  
+    // Call your function to display the data
+    displayCards(details);
+  };
+
+const displayCards = (details) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.textContent = "";
+
+
+  details.forEach((AI) => {
+    const card = document.createElement("div");
+    card.classList = `card w-96 bg-base-100 border-2 border-gray-200 p-5`;
+    
+    card.innerHTML = `
+        <figure class="">
+          <img class="h-[190px]" src="${AI.image}" alt="Image Not Found" class="rounded-xl" />
+        </figure>
+        <div class="mt-4">
+        <h1 class="text-xl font-semibold">Features</h1>
+        <p class="count-track text-gray-500">1. ${AI.features[0]}</p>
+        <p class="count-track text-gray-500">1. ${AI.features[1]}</p>
+        <p class="count-track text-gray-500">1. ${AI.features[2]}</p>
+        <hr>
+        <h1 class="text-xl font-semibold mt-2">Features</h1>
+        <p class="count-track text-gray-500">Published in : ${AI.published_in}</p>
+        </div>
+      `;
+        cardContainer.appendChild(card)
+  });
+};
+
+loadData();

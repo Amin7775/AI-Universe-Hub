995f35d3d04e24e69840bbb3951dc4a4b0a39db6
Amin7775 <amin.dpi24@gmail.com>
2023-09-01 22:20:19 +0600

    Added dynamic sort by date btn

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 const loadData = async () => {
-    // Fetch data from the API
-    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+  // Fetch data from the API
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
 
-    const data = await res.json();
+  const data = await res.json();
 
-    const details = data.data.tools;
-  
-    console.log(typeof details);
-  
-    // Call your function to display the data
-    displayCards(details);
-  };
+  const details = data.data.tools;
 
+  console.log(typeof details);
+
+  // Call your function to display the data
+  displayCards(details);
+  return details;
+};
+
+loadData();
 const displayCards = (details) => {
   const cardContainer = document.getElementById("card-container");
   cardContainer.textContent = "";
 
-
   details.forEach((AI) => {
     const card = document.createElement("div");
     card.classList = `card w-80 lg:w-88 bg-base-100 border-2 border-gray-200 p-5 mx-auto`;
-    
+
     card.innerHTML = `
         <figure class="">
           <img class="h-[190px]" src="${AI.image}" alt="Image Not Found" class="rounded-xl" />
@@ -35,8 +36,26 @@ const displayCards = (details) => {
         <p class="count-track text-gray-500">Published in : ${AI.published_in}</p>
         </div>
       `;
-        cardContainer.appendChild(card)
+    cardContainer.appendChild(card);
   });
 };
 
-loadData();
+const sortBtn = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`);
+
+  const data = await res.json();
+
+  const details = data.data.tools;
+  console.log(details);
+
+  details.sort((a, b) => {
+    let DateA = new Date(a.published_in);
+    let DateB = new Date(b.published_in);
+
+    // return DateB - DateA;
+    return DateA - DateB;
+  });
+
+  // Call the displayCards function to update the displayed content with the sorted data
+  displayCards(details);
+};
